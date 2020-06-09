b962588959a5e77c5d208768ebf293200f55f1a6
Jaga Santagostino <jagasantagostino@gmail.com>
2020-06-09 18:37:23 +0200

    update deps

--- src/services/root.ts
+++ src/services/root.ts
@@ -1,7 +1,8 @@
+import { Plugin } from 'fastify'
 import fp from 'fastify-plugin'
 
-export default fp(async function(fastify, opts) {
-  fastify.get('/', async function(request, reply) {
+export default fp(async function (fastify, opts) {
+  fastify.get('/', async function (request, reply) {
     return { root: true }
   })
 })
